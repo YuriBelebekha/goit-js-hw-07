3bd5fb55ed0578b40d443cec8892a2512fa37911
Yuri Belebekha <ybelebekha@gmail.com>
2022-11-01 23:00:27 +0200

    finished Task 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+  .map(( {preview, original, description} ) => {
+    return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    `
+  })
+  .join('');  
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
